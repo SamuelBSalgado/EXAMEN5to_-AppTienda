59b115cbe53b93a177008dd005abbdbd0391086e
SamBS <fbecerra0@ucol.mx>
2023-11-19 21:32:40 -0600

    listado de productos listo

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ClientsListPage } from '../clients-list/clients-list.page';
+import { ProductsListPage } from '../products-list/products-list.page';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +13,10 @@ export class DashboardPage {
 
   goToClientsList(){
     this.navCtrl.navigateForward('/clients-list');
+  }
+
+  goToProductsList(){
+    this.navCtrl.navigateForward('/products-list');
   }
 
 }
