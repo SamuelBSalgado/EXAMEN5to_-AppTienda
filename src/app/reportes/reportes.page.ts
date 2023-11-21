b7ae16ce27d4afbd5cfbffabb06141be124d680c
SamBS <fbecerra0@ucol.mx>
2023-11-20 22:14:48 -0600

    Add empty reportes interface

--- src/app/reportes/reportes.page.ts
+++ src/app/reportes/reportes.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.page.html',
   styleUrls: ['./reportes.page.scss'],
 })
-export class ReportesPage implements OnInit {
+export class ReportesPage {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
